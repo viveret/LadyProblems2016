32b5c2de809adc87ea66e3cbb8f9cf662e3cefdb
viveret <viveret.amant@gmail.com>
2016-11-05 13:18:22 -0700

    First real commit

---
+++ src/main/java/com/ladyproblems16/app1/MainActivity.java
@@ -0,0 +1,26 @@
+package com.ladyproblems2016.app1;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.content.Intent;
+
+import android.view.Display;
+import android.graphics.Point;
+
+public class MainActivity extends Activity {
+	public void onCreate(Bundle bundle) {
+		super.onCreate(bundle);
+	    //setContentView(R.layout.main_activity);
+	}
+
+
+	@Override
+	protected void onResume() {
+	  super.onResume();
+	}
+
+	@Override
+	protected void onPause() {
+	  super.onPause();
+	}
+}
---
+++ src/main/java/com/ladyproblems16/app1/MyApplication.java
@@ -0,0 +1,13 @@
+package com.ladyproblems2016.app1;
+
+import android.app.Application;
+import android.app.Activity;
+import android.os.Bundle;
+import android.content.pm.ActivityInfo;
+import android.content.Context;
+
+import java.util.HashMap;
+
+public class MyApplication extends Application {
+
+}
