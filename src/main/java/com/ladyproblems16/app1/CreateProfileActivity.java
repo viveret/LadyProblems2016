53a7c60cec02f2b63cbce8534b480c7bb2e2b3a0
viveret <viveret.amant@gmail.com>
2016-11-06 12:03:20 -0800

    Finalizing app

--- src/main/java/com/ladyproblems16/app1/CreateProfileActivity.java
+++ src/main/java/com/ladyproblems16/app1/CreateProfileActivity.java
@@ -15,10 +15,12 @@ import android.widget.Button;
 import android.view.View;
 import android.widget.NumberPicker;
 import android.widget.Spinner;
+import android.widget.ArrayAdapter;
 
 public class CreateProfileActivity extends Activity {
 
     private Button connectWithFacebook;
+    private Button nextButton;
     private NumberPicker selectAgeNumberPicker;
     private Spinner selectGenderSpinner;
     private Spinner selectRaceSpinner;
@@ -30,12 +32,24 @@ public class CreateProfileActivity extends Activity {
 	    setContentView(R.layout.create_profile);
 
         connectWithFacebook = (Button) findViewById(R.id.connect_with_facebook);
+        nextButton = (Button) findViewById(R.id.go_next_button);
+
         selectAgeNumberPicker = (NumberPicker) findViewById(R.id.select_age);
 
         selectGenderSpinner = (Spinner) findViewById(R.id.select_gender);
         selectRaceSpinner = (Spinner) findViewById(R.id.select_race);
 
         userFullName = (EditText) findViewById(R.id.edit_user_full_name);
+
+        String[] ops = getResources().getStringArray(R.array.gender);
+        ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, ops);
+        spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        selectGenderSpinner.setAdapter(spinnerArrayAdapter);
+
+        String[] ops2 = getResources().getStringArray(R.array.ethnicityRace);
+        ArrayAdapter<String> ethnicityRaceArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, ops2);
+        ethnicityRaceArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        selectRaceSpinner.setAdapter(ethnicityRaceArrayAdapter);
 
         // Load user info from somewhere
         // basically myUserDescription = blah.getUserDesc();
@@ -47,22 +61,27 @@ public class CreateProfileActivity extends Activity {
             });
         }
 
-        if (selectAgeNumberPicker != null) {
-            //get value()
-
+        if (UserDescriptionManager.exists(this)) {
+            IUserDescription tmp = UserDescriptionManager.loadFromFile(this);
+            selectGenderSpinner.setSelection(((ArrayAdapter)selectGenderSpinner.getAdapter()).getPosition(tmp.getGender()));
+            selectRaceSpinner.setSelection(((ArrayAdapter)selectRaceSpinner.getAdapter()).getPosition(tmp.getEthnicityRace()));
+            selectAgeNumberPicker.setValue(tmp.getAge());
+            userFullName.setText(tmp.getFullName());
         }
+        
+        if (nextButton != null) {
+            nextButton.setOnClickListener(new Button.OnClickListener() {
+                public void onClick(View v) {
+                    IUserDescription tmp = new UserDescription();
+                    tmp.setFullName(userFullName.getText().toString());
+                    tmp.setGender(selectGenderSpinner.getSelectedItem().toString());
+                    tmp.setEthnicityRace(selectRaceSpinner.getSelectedItem().toString());
+                    tmp.setAge(selectAgeNumberPicker.getValue());
 
-        if (selectGenderSpinner != null) {
-            // store gender
-
-        } 
-
-        if (selectRaceSpinner != null) {
-            // store race
-        }
-
-        if (userFullName != null) {
-            // store name
+                    UserDescriptionManager.writeToFile(CreateProfileActivity.this, tmp);
+                    finish();
+                }
+            });
         }
  	}
 }
--- src/main/java/com/ladyproblems16/app1/HomeActivity.java
+++ src/main/java/com/ladyproblems16/app1/HomeActivity.java
@@ -48,23 +48,6 @@ public class HomeActivity extends Activity {
         editContact = (Button) findViewById(R.id.edit_contact);
         
         call911 = (Button) findViewById(R.id.call911_button);
-
-        // Load user info from somewhere
-        myUserDescription = UserDescriptionManager.loadFromFile(this);
-        if (myUserDescription != null) {
-            if (myUserFullNameTextView != null) {
-                myUserFullNameTextView.setText(myUserDescription.getFullName());
-            }
-            if (myUserFullNameTextView != null) {
-                myUserSubInfoTextView.setText(myUserDescription.getGender() +
-                                              ", " + myUserDescription.getAge());
-            }
-        }
-
-        List<IContact> tmpList = ContactManager.loadFromFile(this);
-        if (tmpList != null && tmpList.size() > 0) {
-            emergencyContact = tmpList.get(0);
-        }
         
         if (editUserDesc != null) {
             editUserDesc.setOnClickListener(new Button.OnClickListener() {
@@ -84,16 +67,6 @@ public class HomeActivity extends Activity {
             });
         }
 
-        if (emergencyContact != null) {
-            if (myContactFullNameTextView != null) {
-                myContactFullNameTextView.setText(emergencyContact.getName());
-            }
-            if (myContactNumberTextView != null) {
-                myContactNumberTextView.setText(emergencyContact.getNumber());
-            }
-        }
-
-        
         if (call911 != null) {
             call911.setOnClickListener(new Button.OnClickListener() {
                 public void onClick(View v) {
@@ -103,13 +76,47 @@ public class HomeActivity extends Activity {
             });
         }
 
+        reloadUserDesc();
+        reloadContact();
         //myGps = new GpsAccess(this);
 	}
 
+    private void reloadUserDesc() {
+        // Load user info from somewhere
+        myUserDescription = UserDescriptionManager.loadFromFile(this);
+        if (myUserDescription != null) {
+            if (myUserFullNameTextView != null) {
+                myUserFullNameTextView.setText(myUserDescription.getFullName());
+            }
+            if (myUserFullNameTextView != null) {
+                myUserSubInfoTextView.setText(myUserDescription.getGender() +
+                                              ", " + myUserDescription.getAge());
+            }
+        }
+    }
+
+    private void reloadContact() {
+        List<IContact> tmpList = ContactManager.loadFromFile(this);
+        if (tmpList != null && tmpList.size() > 0) {
+            emergencyContact = tmpList.get(0);
+        }
+        
+        if (emergencyContact != null) {
+            if (myContactFullNameTextView != null) {
+                myContactFullNameTextView.setText(emergencyContact.getName());
+            }
+            if (myContactNumberTextView != null) {
+                myContactNumberTextView.setText(emergencyContact.getNumber());
+            }
+        }
+    }
 
 	@Override
 	protected void onResume() {
 	  super.onResume();
+
+        reloadUserDesc();
+        reloadContact();
       //myGps.resume();
 	}
 
